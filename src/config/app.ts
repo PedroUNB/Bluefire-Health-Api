2006b3abd9d5ba9d6b2d050be25a1835a54a1230
pedro <pedro.oliveira.unb@gmail.com>
2021-02-19 00:02:24 -0300

    :loud_sound: Configuração dos Middlewares

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,4 +1,6 @@
+import cors from 'cors'
 import express, { Application } from 'express'
+import morgan from 'morgan'
 import Controllers from './routes'
 class App {
   public express: Application
@@ -6,7 +8,14 @@ class App {
   constructor() {
     this.express = express()
 
+    this.middlewares()
     this.routes(Controllers)
+  }
+
+  private middlewares() {
+    this.express.use(express.json())
+    this.express.use(cors({ origin: '*' }))
+    this.express.use(morgan('[:method] :remote-addr :status ":url" :response-time ms'))
   }
 
   private routes(Controllers: { forEach: (arg0: (Controller: any) => void) => void; }) {
