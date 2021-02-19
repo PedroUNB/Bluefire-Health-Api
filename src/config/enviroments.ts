06d26c28d9376f091c11574dcc28f530b45e450b
pedro <pedro.oliveira.unb@gmail.com>
2021-02-18 23:48:10 -0300

    :globe_with_meridians: Configuração das variaveis de ambiente

---
+++ src/config/enviroments.ts
@@ -0,0 +1,7 @@
+import { config } from 'dotenv'
+
+config()
+
+export const port = process.env.PORT || 3000
+export const enviroment = process.env.ENVIROMENT || null
+export const apiUrl = process.env.API_URL || `http://127.0.0.1:${process.env.PORT || 3000}`
